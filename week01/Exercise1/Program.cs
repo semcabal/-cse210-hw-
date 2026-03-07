d1872cc4b87a545d789be41e7afa3056af98806a
semcabal <dhcars3@gmail.com>
2026-03-06 21:01:05 -0500

    Changing a variable for CamelCase

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,10 +6,10 @@ class Program
     {
         Console.Write("What is your first name? ");
         string name = Console.ReadLine();
-        
-        Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last_name}, {name} {last_name}.");
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LastName}, {name} {LastName}.");
     }
 }
