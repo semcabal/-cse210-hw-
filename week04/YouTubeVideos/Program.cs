07311ec7d1ee8a7e08651bce51c46872de27386b
semcabal <dhcars3@gmail.com>
2026-03-28 12:21:30 -0500

    Work of Youtube

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        // Video 1
+        Video video1 = new Video("C# Basics", "John", 300);
+        video1.AddComment(new Comment("Ana", "Very helpful!"));
+        video1.AddComment(new Comment("Luis", "Nice explanation"));
+        video1.AddComment(new Comment("Maria", "I learned a lot"));
+        videos.Add(video1);
+
+        // Video 2
+        Video video2 = new Video("OOP in C#", "Carlos", 450);
+        video2.AddComment(new Comment("Pedro", "Great video"));
+        video2.AddComment(new Comment("Lucia", "Clear and simple"));
+        video2.AddComment(new Comment("Jose", "Thanks!"));
+        videos.Add(video2);
+
+        // Video 3
+        Video video3 = new Video("Lists in C#", "Sofia", 200);
+        video3.AddComment(new Comment("Miguel", "Good examples"));
+        video3.AddComment(new Comment("Elena", "Easy to understand"));
+        video3.AddComment(new Comment("Raul", "Helpful"));
+        videos.Add(video3);
+
+        // Mostrar información
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length: {video.Length} seconds");
+            Console.WriteLine($"Comments: {video.GetCommentCount()}");
+
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.Name}: {comment.Text}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; }
+
+    private List<Comment> comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return comments;
+    }
+}
